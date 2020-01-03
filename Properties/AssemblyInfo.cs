00791bfeb3234a983cc8005c108c5cd4c84b42a7
kevL <k3v1s73n@gmail.com>
2020-01-03 01:24:22 -0700

    2.0.0.0 - 2020

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,8 +11,8 @@ using System.Runtime.InteropServices;
 #endif
 [assembly: AssemblyCompany("kL")]
 [assembly: AssemblyProduct("CreatureVisualizer")]
-[assembly: AssemblyCopyright("Copyleft 2019")]
+[assembly: AssemblyCopyright("Copyleft 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.9.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
