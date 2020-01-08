e1394c7be00e9379fa4e812e3fd388312856eaa9
kevL <k3v1s73n@gmail.com>
2020-01-08 00:03:31 -0700

    statusbars - don't draw vertical borderline

--- StripRenderer.cs
+++ StripRenderer.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 
@@ -21,8 +20,7 @@ namespace creaturevisualizer
 
 		protected override void OnRenderToolStripBorder(ToolStripRenderEventArgs e)
 		{
-			e.Graphics.DrawLine(Pens.Black, e.ToolStrip.Width, 0, 0,0);
-			e.Graphics.DrawLine(Pens.Black, 0,0, 0, e.ToolStrip.Height);
+			e.Graphics.DrawLine(Pens.Black, 0,0, e.ToolStrip.Width, 0);
 		}
 	}
 }
